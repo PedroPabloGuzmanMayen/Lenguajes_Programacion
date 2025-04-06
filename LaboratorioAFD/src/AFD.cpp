dcb56491b5f33cb9b0424f37d1bf0829b667cca1
donmatthiuz <cor22982@uvg.edu.gt>
2025-04-05 22:28:55 -0600

    fixing numbers

--- LaboratorioAFD/src/AFD.cpp
+++ LaboratorioAFD/src/AFD.cpp
@@ -133,7 +133,7 @@
                 tokens.emplace_back(tipoToken, tokenLexema);
                 i = lastAcceptPos + 1;
             } else {
-                std::string tokenLexema(1, entrada[j]);
+                std::string tokenLexema = entrada.substr(i, lastAcceptPos - i + 1);
                std::cerr << " ⚠️  Error: No se pudo reconocer el token en la posición " << i << " lexema: "<< entrada[j]<<"\n"<<std::endl;
                tokens.emplace_back("Error", tokenLexema);
                i++;
